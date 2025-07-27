bebe2b85fdf12cb13b65dde53392aeb7b9c85bbe
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-07-26 21:24:18 -0400

    feat(dag): add new tasks to the dag

--- dags/dag.py
+++ dags/dag.py
@@ -11,7 +11,7 @@ from scripts.extract.extract_users import extract_to_csv as csv
 
 default_args = {
     'owner': 'CamilaJaviera',
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2024, 7, 26),
     'depends_on_past': False,
     'retries': 1,
 }
@@ -28,9 +28,19 @@ with DAG(
         bash_command='dbt run --select staging.users --profiles-dir /opt/airflow/dbt_project --project-dir /opt/airflow/dbt_project'
     )
 
+    task_products = BashOperator(
+        task_id='run_dbt_products',
+        bash_command='dbt run --select staging.products --profiles-dir /opt/airflow/dbt_project --project-dir /opt/airflow/dbt_project'
+    )
+
+    task_orders = BashOperator(
+        task_id='run_dbt_orders',
+        bash_command='dbt run --select staging.orders --profiles-dir /opt/airflow/dbt_project --project-dir /opt/airflow/dbt_project'
+    )
+
     task_csv = PythonOperator(
         task_id='extract_to_csv',
         python_callable=csv,
     )
 
-    task_users >> task_csv # type: ignore
+    task_users >> task_products >> task_orders >> task_csv # type: ignore
