f89b5dbc5c7ba1f05572fabab0c651d01441fee1
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-07-26 21:22:15 -0400

    feat(models): delete role, create and terminated. Also change country by city

--- dbt_project/models/staging/users.sql
+++ dbt_project/models/staging/users.sql
@@ -17,17 +17,10 @@ last_names AS (
         'Vargas', 'Silva', 'Torres', 'Rojas', 'Flores', 'Morales', 'Ortega', 'Herrera', 'Reyes', 'Fuentes'
     ]) WITH ORDINALITY AS t(last_name, id)
 ),
-roles AS (
+city AS (
     SELECT *
     FROM unnest(ARRAY[
-        'Data Analyst', 'Data Engineer', 'BI Analyst', 'Software Developer', 'Project Manager',
-        'Marketing Specialist', 'QA Engineer', 'Support Engineer', 'HR Specialist', 'Finance Analyst'
-    ]) WITH ORDINALITY AS t(role, id)
-),
-countries AS (
-    SELECT *
-    FROM unnest(ARRAY[
-        'Chile', 'Argentina', 'Colombia', 'México', 'Perú', 'Uruguay', 'Paraguay', 'Ecuador', 'Bolivia', 'Venezuela'
+        'Curicó', 'Talca', 'Linares', 'San Fernando', 'Santiago', 'Viña del Mar', 'Puerto Montt', 'Rancagua', 'Valparaiso', 'Parral'
     ]) WITH ORDINALITY AS t(country, id)
 ),
 base AS (
@@ -36,8 +29,7 @@ base AS (
         (random() * interval '3 years') AS random_date,
         floor(random() * 20 + 1)::int AS fn_id,
         floor(random() * 20 + 1)::int AS ln_id,
-        floor(random() * 10 + 1)::int AS role_id,
-        floor(random() * 10 + 1)::int AS country_id,
+        floor(random() * 10 + 1)::int AS city_id,
         CASE 
             WHEN random() > 0.5 THEN 'active'
             ELSE 'inactive'
@@ -53,7 +45,6 @@ user_data AS (
             THEN 'F'
             ELSE 'M'
         END AS gender,
-        r.role,
         c.country,
         CONCAT(lower(regexp_replace(fn.first_name, '[^a-zA-Z]', '', 'g')), '_',
                lower(regexp_replace(ln.last_name, '[^a-zA-Z]', '', 'g')), '_', b.user_id) AS username,
@@ -61,17 +52,11 @@ user_data AS (
             regexp_replace(CONCAT(fn.first_name, '.', ln.last_name, b.user_id, '@example.com'), '[^a-zA-Z0-9.@]', '', 'g')
         ) AS email,
         CONCAT('+56 9 ', LPAD((floor(random() * 100000000)::int)::text, 8, '0')) AS phone_number,
-        now() - b.random_date AS created_at,
-        b.status,
-        CASE
-            WHEN b.status = 'active' THEN NULL
-            ELSE b.random_date + now()
-        END AS terminated_at
+        b.status
     FROM base b
     LEFT JOIN first_names fn ON b.fn_id = fn.id
     LEFT JOIN last_names ln ON b.ln_id = ln.id
-    LEFT JOIN roles r ON b.role_id = r.id
-    LEFT JOIN countries c ON b.country_id = c.id
+    LEFT JOIN city c ON b.city_id = c.id
 )
 
 SELECT * FROM user_data
