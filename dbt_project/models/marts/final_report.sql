04911a53a5476108c64fae44c8eae806301130a3
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-07-29 17:06:42 -0400

    feat(sql): change user by client

--- dbt_project/models/marts/final_report.sql
+++ dbt_project/models/marts/final_report.sql
@@ -6,14 +6,14 @@
 WITH final_report AS (
     SELECT 
         o.order_id AS order_id, 
-        u.user_id AS user_id,
-        CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+        c.client_id AS user_id,
+        CONCAT(c.first_name, ' ', c.last_name) AS full_name,
         DATE(o.sale_date) AS sale_date,
         p.product_name AS product_name,
         p.category AS product_category,
         p.price AS product_price
     FROM data.orders o
-    JOIN data.users u ON u.user_id = o.client_id
+    JOIN data.clients c ON c.client_id = o.client_id
     JOIN data.products p ON p.product_id = o.product_id
 )
 
